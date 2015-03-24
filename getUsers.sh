97f6e57d4d00e589485b549d42afadea369180b1
Michael Große <mic.grosse+github@googlemail.com>
2015-03-24 23:31:23 +0100

    Remove dead code

--- getUsers.sh
+++ getUsers.sh
@@ -3,13 +3,3 @@ regex='[-[:alnum:]]*:[[:alnum:]]*:[0-9]*:[0-9]*:[-[:alpha:]]* [-[:alpha:]]*,*:/h
 egrep "$regex" /etc/passwd | cut -d: -f1,5,7 > Benutzerdaten.txt
 
 return
-while read line
-do
-  #IFS=':' read -a array <<< "$line"
-  array=( ${line//:/ } )
-  echo ${array[0]}
-  echo ${array[4]}
-  echo ${array[6]}
-  echo "\n"
-done < temp.txt
-rm temp.txt
